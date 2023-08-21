9cd4e2f47a22b88d7cbfcd76d977b7847556d78c
Abdulkareem Aldeek <69352357+DeekCS@users.noreply.github.com>
2023-08-21 12:12:59 +0300

    feat: change port

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
