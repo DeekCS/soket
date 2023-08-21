116f842dbafcb2e80a37411e56b5120ac270216e
Abdulkareem Aldeek <69352357+DeekCS@users.noreply.github.com>
2023-08-21 12:09:25 +0300

    feat: hanlde rooms

--- server.js
+++ server.js
@@ -11,11 +11,19 @@ const io = socketIo(server);
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
+  // Handle joining a room
+  socket.on("joinRoom", (roomId) => {
+    socket.join(roomId); // Join the specified room
+    console.log(`User ${socket.id} joined room ${roomId}`);
+  });
+
   // Handle location updates from clients
   socket.on("sendLocation", (data) => {
     console.log("got data,", data);
-    // Broadcast the location to all connected clients except the sender
-    socket.broadcast.emit("receiveLocation", data);
+    // Broadcast the location to all clients in the same room
+    io.to(data?.roomId).emit("receiveLocation", data);
+
+
   });
 
   socket.on("disconnect", () => {
